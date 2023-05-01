126f7167cdfb8ca10f2222ca48150355b1440ebe
chaosknight <chaos.closure@gmail.com>
2023-05-01 17:49:34 +0800

    精简代码

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "encoding/json"
 	"log"
 	"net/http"
 	"time"
@@ -10,56 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// var Insts_ = []*entity.FinesseConfig{
-// 	&entity.FinesseConfig{
-// 		InstID:      "DOGE-USDT-SWAP",
-// 		Firstcount:  10,
-// 		Maxcount:    2000,
-// 		Addposition: -10,
-// 		Maxloss:     2,
-// 	},
-// 	&entity.FinesseConfig{
-// 		InstID:      "OP-USDT-SWAP",
-// 		Firstcount:  1,
-// 		Maxcount:    100,
-// 		Addposition: -10,
-// 		Maxloss:     2,
-// 	},
-// }
-// var Lginuser_ = []string{
-// 	"541bba76-8d22-45a4-b370-91d763d58055",
-// 	"Chaos_256398",
-// 	"92A8CABC7A56D45E9FE4B2296FC601A5",
-// }
-
 func main() {
-	// cnf := entity.Sysconfig{
-	// 	User: [3]string{"541bba76-8d22-45a4-b370-91d763d58055",
-	// 		"Chaos_256398",
-	// 		"92A8CABC7A56D45E9FE4B2296FC601A5"},
-	// 	Insts: []*entity.FinesseConfig{
-	// 		&entity.FinesseConfig{
-	// 			InstID:      "DOGE-USDT-SWAP",
-	// 			Firstcount:  10,
-	// 			Maxcount:    2000,
-	// 			Addposition: -10,
-	// 			Maxloss:     2,
-	// 		},
-	// 		&entity.FinesseConfig{
-	// 			InstID:      "OP-USDT-SWAP",
-	// 			Firstcount:  1,
-	// 			Maxcount:    100,
-	// 			Addposition: -10,
-	// 			Maxloss:     2,
-	// 		},
-	// 	},
-	// }
-
-	// jsonstr, err := json.Marshal(cnf)
-	// if err == nil {
-	// 	log.Println(string(jsonstr))
-	// }
-
 	syscnf := entity.LoadCnf("./cfg.json")
 	if syscnf == nil {
 		log.Println("配置文件错误")
